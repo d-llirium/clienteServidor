8b22fc3e9160375b6a4ceded17396eca17e0d863
d-llirium <plimacarlos@gmail.com>
2022-05-01 13:22:56 -0300

    Altere o servidor para que ele retorne ao cliente uma confirmação de que recebeu a mensagem enviada pelo cliente. Altere o cliente para receber uma confirmação do servidor a cada mensagem enviada.

--- client/Client.java
+++ client/Client.java
@@ -1,6 +1,8 @@
 package client;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 // aplicação Cliente
 public class Client {
@@ -8,6 +10,8 @@ public class Client {
         final String IP = "127.0.0.1"; //IP que se refere a proópria máquina, como o THIS
         final int PORT = 12345; // precisa ser o mesmo número da porta do servidor
         Socket socket;
+        PrintStream output = null;
+        Scanner input = null;
 
         // criação do socket a pedido de conexão
         try {
@@ -18,10 +22,21 @@ public class Client {
         }
 
         // fase de comunicação , troca de dados
+        try {
+            output = new PrintStream(socket.getOutputStream()); // para escrever para o servidor
+            input = new Scanner(socket.getInputStream()); // para ler a mensagem do servidor
+            output.println("Boa noite SERVIDOR <3");
+            String msg = input.nextLine(); // recebe a msg do input
+            System.out.println("Recebido: " + msg);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         // fase de encerramento da conexão
         try {
+            output.close();
             socket.close();
+            System.out.println("Acabou a conexão do CLIENTE");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- server/Server.java
+++ server/Server.java
@@ -1,7 +1,9 @@
 package server;
 
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 // aplicação Servidor
 public class Server { // cada vez que se roda abre um novo servidor
@@ -9,6 +11,8 @@ public class Server { // cada vez que se roda abre um novo servidor
         final int PORT = 12345;
         ServerSocket serverSocket;
         Socket clientSocket = null;
+        Scanner input = null;
+        PrintStream output = null;
 
         // criar o socket e fazer bind 
         try { // tratar excessão
@@ -22,16 +26,32 @@ public class Server { // cada vez que se roda abre um novo servidor
         try {
             System.out.println("Aguardando pedido de conexão...");
             clientSocket = serverSocket.accept(); // para e fica esperando a conexão... RETORNA o client socket
+
             System.out.println("Conectado com " + clientSocket.getInetAddress().getHostAddress());
+
         } catch (Exception e) {
             System.out.println("Erro na conexão...");
             System.out.println(e.getMessage());
         }
+
         // fase da comunicação = troca de dados
+        try {
+            input = new Scanner(clientSocket.getInputStream()); // para ler a mensagem do cliente
+            output = new PrintStream(clientSocket.getOutputStream()); // para escrever mensagem para o cliente
+            
+            String msg = input.nextLine(); // recebe a msg do input
+            System.out.println("Recebido: " + msg);
+            output.println("Boa noite CLIENTE!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         // fase de encerramento da conexão
         try {
+            input.close();
             clientSocket.close();
             serverSocket.close();
+            System.out.println("Acabou a conexão do SERVIDOR");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
